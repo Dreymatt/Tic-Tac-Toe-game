2d90168aaa9167f6dded369f180abdb30007f5bd
Dare Matthew Kugblenu <darekugblenu@gmail.com>
2026-01-06 06:49:19 +0100

    first commit of the day

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 
 
-window.onload = function() {
+/*window.onload = function() {
   const request = new XMLHttpRequest();
 
-  request.open('GET', 'https://api.thecatapi.com/v1/images/search?limit=10', false);
+  request.open('GET', 'https://api.unsplash.com/photos/random?count=10&client_id=j43GRwJ8IAvxsJ06oftve4bgyd2E3hsJIjJX3sPYpxA', false);
 
   request.send();
 
   const response = JSON.parse(request.responseText);
-
+  console.log(response)
   const container = document.getElementById('container');
 
   container.innerHTML = `
@@ -16,4 +16,20 @@ window.onload = function() {
       return `<img src="${cat.url}" />`
   })}
   `;
+}*/
+const container = document.getElementById('container');
+const noOfImages = document.getElementById('noOfImages');
+const btn = document.getElementById('btn');
+
+async function getPicture() {
+  try {
+    const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=' + noOfImages.value);
+    const catPicture = response.json();
+    console.log(catPicture)
+    container.innerHTML = `${`<img src="${catPicture.url}"/>`}`;
+  } catch (err) {
+    console.log(err);
+  }
 }
+
+btn.addEventListener('click', getPicture)
