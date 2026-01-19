a707e762364b31030505c22da7f2543f1ff49e51
Dare Matthew Kugblenu <darekugblenu@gmail.com>
2026-01-19 13:12:54 +0100

    My second commit of the struggle

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
+const container = document.getElementById('container');
+const noOfImages = document.getElementById('noOfImages');
+const btn = document.getElementById('btn');
 
-
-/*window.onload = function() {
+ function getPicture() {
   const request = new XMLHttpRequest();
 
-  request.open('GET', 'https://api.unsplash.com/photos/random?count=10&client_id=j43GRwJ8IAvxsJ06oftve4bgyd2E3hsJIjJX3sPYpxA', false);
+  request.open('GET',`https://api.thecatapi.com/v1/images/search?limit=${noOfImages.value}`, false);
 
   request.send();
 
@@ -16,20 +18,24 @@
       return `<img src="${cat.url}" />`
   })}
   `;
-}*/
-const container = document.getElementById('container');
-const noOfImages = document.getElementById('noOfImages');
-const btn = document.getElementById('btn');
-
-async function getPicture() {
-  try {
-    const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=' + noOfImages.value);
-    const catPicture = response.json();
-    console.log(catPicture)
-    container.innerHTML = `${`<img src="${catPicture.url}"/>`}`;
-  } catch (err) {
-    console.log(err);
-  }
 }
+// const container = document.getElementById('container');
+// const noOfImages = document.getElementById('noOfImages');
+// const btn = document.getElementById('btn');
+
+// async function getPicture() {
+//   try {
+//     const response = await fetch(`https://api.thecatapi.com/v1/images/search?api_key=live_u670Pzg1bwPwNHOOQqJVNs3721S8Y4sB4M8Kvlp0skF4LDbQf3Bu69DP0jjzkPWE&limit=${noOfImages.value}`);
+//     const catPicture = response.json;
+//     console.log(catPicture)
+//     container.innerHTML =  `${`<img src = "${catPicture.url}"`}`;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
 
 btn.addEventListener('click', getPicture)
+window.onclick = function(){
+  console.log("Window was interacted with");
+};
+
